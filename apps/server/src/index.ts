44433027f48ad15ab4fd9847ca53e3957d59d28a
Valentin NAHIM <vnahim@pictet.com>
2026-04-21 13:34:04 +0200

    add server vercel

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -12,7 +12,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://op-tcg-simu-client.vercel.app/'],
     methods: ['GET', 'POST'],
   },
 });
